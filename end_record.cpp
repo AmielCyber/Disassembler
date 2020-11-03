e6210094b60f7685e51c3edf3871b99b0bf5e242
peanutcelery <67120075+peanutcelery@users.noreply.github.com>
2020-11-02 21:41:45 -0800

    Update end_record.cpp

--- end_record.cpp
+++ end_record.cpp
@@ -21,30 +21,4 @@ void parse_end_line(string line, Disassembler *disassembler) {
     }
 
 
-void parse_end_line(string line, Disassembler *disassembler)
-{
-  if(line.compare(0, 1, "E") == 0)
-  {
-    int length = line.length();
-
-    if(length <= 7)
-    {
-      string startAddress = line.substr(1, length - 1);
-      disassembler->addEnd(startAddress);
-    }
-    else
-    {
-      cout << "Invalid line" << endl;
-      exit(EXIT_FAILURE);
-    }
-    
-    
-  }
-  else
-  {
-    cout << "Invalid line" << endl;
-    exit(EXIT_FAILURE);
-  }
-  
-  
 }
