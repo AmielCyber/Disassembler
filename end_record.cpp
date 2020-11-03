e069de4fbf98b58470f14dd915899671faca8f6b
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2020-11-02 21:12:03 -0800

    remove if line == 7

--- end_record.cpp
+++ end_record.cpp
@@ -10,16 +10,9 @@ void parse_end_line(string line, Disassembler *disassembler)
   if(line.compare(0, 1, "E") == 0)
   {
     int length = line.length();
-    if(length == 7)
-    {
       string startAddress = line.substr(1, length - 1);
       disassembler->addEnd(startAddress);
-    }
-    else
-    {
-      cout << "Invalid line" << endl;
-      exit(EXIT_FAILURE);
-    }
+    
   }
   else
   {
